af78635ad6276bd8de61552871533212b0d16ba2
Esther Guo <special0831qi@gmail.com>
2019-12-01 11:28:45 +0800

    for element in list

---
+++ chapter4-working with lists/4.1-pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ['Hawaii', 'BBQ', 'Bacon']
+
+for pizza in pizzas:
+    print(f'I like {pizza} pizza.')
+
+print('I really love pizza!')
---
+++ chapter4-working with lists/4.2-animals.py
@@ -0,0 +1,6 @@
+animals = ['dog', 'bird', 'horse']
+
+for animal in animals:
+    print(f'A {animal} would make a great pet!')
+
+print(f'Any of these animals would make a great pet!')
