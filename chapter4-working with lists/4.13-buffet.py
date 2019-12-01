74e48bdb905dd44ed280f7d1c83a65b56e937aa2
Esther Guo <special0831qi@gmail.com>
2019-12-01 16:48:08 +0800

    tuple

---
+++ chapter4-working with lists/4.13-buffet.py
@@ -0,0 +1,9 @@
+foods = ('bread', 'salad', 'pizza', 'bubble tea', 'coca cola')
+for food in foods:
+    print(food)
+
+# foods[1] = 'ooo' # NNNNNNNNOOOOO
+
+foods = ('bread', 'salad', 'pizza', 'milk', 'coffee')
+for food in foods:
+    print(food)
