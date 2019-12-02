1c0f9f4d1c307ad65f352264193fad900fdb2cd9
Esther Guo <special0831qi@gmail.com>
2019-12-02 23:38:54 +0800

    if statement

---
+++ chapter5-if_statements/5.7-favorate_fruit.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['watermelon', 'peach', 'orange']
+
+if 'watermelon' in favorite_fruits:
+    print('You really like watermelon!')
+if 'peach' in favorite_fruits:
+    print('You really like peach!')
+if 'orange' in favorite_fruits:
+    print('You really like orange!')
+if 'apple' in favorite_fruits:
+    print('You really like apple!')
+if 'passionfruit' in favorite_fruits:
+    print('You really like passionfruit!')
--- chapter5-if_statements/notes.md
+++ chapter5-if_statements/notes.md
@@ -1,2 +1,4 @@
 To find out whether a particular value is already in a list, use the key
 word **in**.  
+**if-elif-else**  
+
